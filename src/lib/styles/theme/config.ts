3aa9b9d29651fe95f47936a94eab7ec2ca68b318
Tom Tran <banhaclong20@gmail.com>
2023-01-04 15:14:43 -0600

    fix: Added useSystemColorMode

--- src/lib/styles/theme/config.ts
+++ src/lib/styles/theme/config.ts
@@ -3,4 +3,5 @@ import type { ThemeConfig } from "@chakra-ui/react";
 export const config: ThemeConfig = {
   initialColorMode: "light",
   disableTransitionOnChange: false,
+  useSystemColorMode: false,
 };
