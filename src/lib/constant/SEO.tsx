23a99fffbd1712a9673646cb9199921e95853d05
Tom Tran <banhaclong20@gmail.com>
2023-01-07 17:00:35 -0600

    feat: Using own SEO config

---
+++ src/lib/constant/SEO.tsx
@@ -0,0 +1,15 @@
+/**
+ * SEO constants
+ */
+
+export const SEO_TITLE =
+  "Oishii - DFW's premier Sushi and Pan-Asian Restaurant";
+export const SEO_DESCRIPTION =
+  "Best Sushi and Pan-Asian Restaurant in Dallas | Plano, TX";
+export const SEO_KEYWORDS =
+  "premier sushi, sushi dallas, sushi plano, sushi frisco, sushi dfw, dallas sushi, plano sushi, frisco sushi, dfw sushi";
+export const SEO_OG_IMAGE =
+  "https://oishii-sushi-web.vercel.app/assets/menu/SalmonTacos.jpg";
+export const SEO_TWITTER_IMAGE =
+  "https://oishii-sushi-web.vercel.app/assets/menu/SalmonTacos.jpg";
+export const PRODUCTION_URL = "https://oishiirestaurants.com";
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { NextSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-import defaultSEOConfig from "../../next-seo.config";
 import { wrapper } from "../store/store";
 import { Chakra } from "lib/components/Chakra";
 import Layout from "lib/layout";
+import { SEO } from "lib/layout/SEO";
 import "lib/styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
@@ -15,13 +14,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <Chakra>
+      <SEO />
       <Head>
         <meta charSet="utf-8" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
         />
-        <NextSeo {...defaultSEOConfig} useAppDir />
       </Head>
 
       {pathname.includes("admin") ? (
